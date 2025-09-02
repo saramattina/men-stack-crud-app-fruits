a53b9f71e2931b2e80df45210a127217bbf96780
Sara Mattina <smattina@gmail.com>
2025-09-02 14:03:07 -0400

    set up boilerplate server code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+   res.render("index.ejs");
+ });
+
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
